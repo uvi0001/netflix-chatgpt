51b67dd49c9d8163108f3eedd683e5254c9a796e
Yuvraj <kokareyuvraj52@gmail.com>
2024-12-20 13:52:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-center p-5 text-3xl font-bold text-green-800">
+     Namaste Everyone, Let's Build Netflix-GPT
+    </div>
+  );
+}
+
+export default App;
